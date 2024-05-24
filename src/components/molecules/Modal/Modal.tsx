85fe3f3da7b1bd0a0871114068d02ec15f62e03e
Sami Kalammallah <skalexsong@gmail.com>
2024-05-25 00:38:57 +0700

    feat: add modal component

---
+++ src/components/molecules/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import {
+  Modal as NextUiModal,
+  ModalContent as NextUiModalContent,
+  ModalBody as NextUiModalBody,
+  ModalFooter as NextUiModalFooter,
+  ModalHeader as NextUiModalHeader,
+  useModal as NextUiUseModal,
+} from "@nextui-org/modal";
+
+import type {
+  ModalProps as NextUiModalProps,
+  ModalContentProps as NextUiModalContentProps,
+  ModalBodyProps as NextUiModalBodyProps,
+  ModalFooterProps as NextUiModalFooterProps,
+  ModalHeaderProps as NextUiModalHeaderProps,
+} from "@nextui-org/modal";
+
+export const Modal = NextUiModal;
+export const ModalContent = NextUiModalContent;
+export const ModalBody = NextUiModalBody;
+export const ModalFooter = NextUiModalFooter;
+export const ModalHeader = NextUiModalHeader;
+export const useModal = NextUiUseModal;
+
+export type ModalProps = NextUiModalProps;
+export type ModalContentProps = NextUiModalContentProps;
+export type ModalBodyProps = NextUiModalBodyProps;
+export type ModalFooterProps = NextUiModalFooterProps;
+export type ModalHeaderProps = NextUiModalHeaderProps;
