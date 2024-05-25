70c3e691265bc348a2d662b67394a828434fd648
Yazid Nurfadil <yazidnurfadil@gmail.com>
2024-05-25 22:23:16 +0700

    chore: add checkbox component

--- src/components/atoms/Card/Card.tsx
+++ src/components/atoms/Card/Card.tsx
@@ -1,19 +1,3 @@
-// // export types
-// export type {CardProps} from "./card";
-// export type {CardFooterProps} from "./card-footer";
-
-// // export hooks
-// export {useCard} from "./use-card";
-
-// // export context
-// export {CardProvider, useCardContext} from "./card-context";
-
-// // export components
-// export {default as Card} from "./card";
-// export {default as CardHeader} from "./card-header";
-// export {default as CardBody} from "./card-body";
-// export {default as CardFooter} from "./card-footer";
-
 import {
   // component
   Card as NextUiCard,
