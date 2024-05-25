fe1904d79a4913588df3318180d55aea921756ce
Sami Kalammallah <skalexsong@gmail.com>
2024-05-25 23:32:08 +0700

    feat: add spacer component

---
+++ src/components/atoms/Spacer/Spacer.tsx
@@ -0,0 +1,7 @@
+import { Spacer as NextUISnippet } from "@nextui-org/spacer";
+
+// export types
+export type { SpacerProps } from "@nextui-org/spacer";
+
+// export component
+export const Spacer = NextUISnippet;
