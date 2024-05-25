5d02becc009be7475f0a3731b647183483fed3e1
Sami Kalammallah <skalexsong@gmail.com>
2024-05-25 23:28:21 +0700

    feat: add snippet component

---
+++ src/components/atoms/Snippet/Snippet.tsx
@@ -0,0 +1,13 @@
+import {
+  Snippet as NextUISnippet,
+  useSnippet as useNextUISnippet,
+} from "@nextui-org/snippet";
+
+// export types
+export type { SnippetProps } from "@nextui-org/snippet";
+
+// export hooks
+export const useSnippet = useNextUISnippet;
+
+// export component
+export const Snippet = NextUISnippet;
