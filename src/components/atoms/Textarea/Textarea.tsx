2f9a67836873d1f901eb0a62208b1b3d78ce4641
Sami Kalammallah <skalexsong@gmail.com>
2024-05-26 00:42:19 +0700

    feat: add textarea component

---
+++ src/components/atoms/Textarea/Textarea.tsx
@@ -0,0 +1,6 @@
+import { Textarea as NextUiTextarea } from "@nextui-org/input";
+import type { TextAreaProps as NextUiTextAreaProps } from "@nextui-org/input";
+
+export type TextAreaProps = NextUiTextAreaProps;
+
+export const Textarea = NextUiTextarea;
