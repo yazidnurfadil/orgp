ee4a9a5608a92a9a1b43000ecccb420f87a91024
Sami Kalammallah <skalexsong@gmail.com>
2024-05-25 23:46:20 +0700

    feat: add switch component

---
+++ src/components/atoms/Switch/Switch.tsx
@@ -0,0 +1,11 @@
+import {
+  Switch as NextUiSwitch,
+  useSwitch as NextUiUseSwitch,
+} from "@nextui-org/switch";
+
+import type { SwitchProps as NextUiSwitchProps } from "@nextui-org/switch";
+
+export type SwitchProps = NextUiSwitchProps;
+
+export const Switch = NextUiSwitch;
+export const useSwitch = NextUiUseSwitch;
